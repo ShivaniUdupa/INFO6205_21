f2596cf4ec515feffa211ca1ccb1d97623f43e52
Devesh Kandpal <kandpal.d@husky.neu.edu>
2017-12-10 16:48:04 -0500

    added trifunction.java

---
+++ Final-Project/src/main/java/udf/TriFunction.java
@@ -0,0 +1,20 @@
+package udf;
+
+import java.util.Objects;
+import java.util.function.Function;
+
+/**
+ * Created by deveshkandpal on 12/7/17.
+ *
+ * Java supports only function and bifunction
+ * TriFunction is a custom functional interface
+ * that defines an apply method which accepts
+ * 3 arguments A,B,C and return R
+ *
+ */
+@FunctionalInterface
+public interface TriFunction<A,B,C,R> extends UserDefinedFunction {
+
+    R apply(A a, B b, C c);
+
+}
